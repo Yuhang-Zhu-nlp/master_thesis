72675e99095c66dcafbc7789a3b0ce82c6a1a58c
Yuhang Zhu <969135594@qq.com>
2024-03-15 15:08:10 +0100

    data extractor v2: remove some low-quality corpus

--- main.py
+++ main.py
@@ -4,12 +4,22 @@ from Corpus_builder.Extractor import (extract_sentence_have,
                                       extract_comparison)
 
 tree_bank_path = '/Users/yuhangzhu/Downloads/Universal Dependencies 2/ud-treebanks-v2.13'
-En_UD = CorpusLoader('English', tree_bank_path, mode='train')
-Zh_UD = CorpusLoader('Chinese', tree_bank_path, mode='train')
-Fi_UD = CorpusLoader('Finnish', tree_bank_path, mode='train', corpus_filter=['UD_Finnish-FTB'])
+En_UD = CorpusLoader('English', tree_bank_path, mode='dev', corpus_filter=['UD_English-Atis', 'UD_English-ESLSpok',
+                                                                             'UD_English-GENTLE', 'UD_English-GUMReddit',
+                                                                             'UD_English-LinES', 'UD_English-ParTUT',
+                                                                             'UD_English-Pronouns', 'UD_English-PUD'])
+Zh_UD = CorpusLoader('Chinese', tree_bank_path, mode='dev', corpus_filter=['UD_Chinese-Beginner', 'UD_Chinese-CFL',
+                                                                             'UD_Chinese-GSDSimp', 'UD_Chinese-HK',
+                                                                             'UD_Chinese-PatentChar'])
+Fi_UD = CorpusLoader('Finnish', tree_bank_path, mode='dev', corpus_filter=['UD_Finnish-FTB', 'UD_Finnish-OOD'])
 Sw_UD = CorpusLoader('Swedish', tree_bank_path, mode='train')
-It_UD = CorpusLoader('Italian', tree_bank_path, mode='train')
-Fr_UD = CorpusLoader('French', tree_bank_path, mode='dev')
+It_UD = CorpusLoader('Italian', tree_bank_path, mode='train', corpus_filter=['UD_Italian-MarkIT', 'UD_Italian-Old',
+                                                                             'UD_Italian-ParlaMint', 'UD_Italian-TWITTIRO',
+                                                                             'UD_Italian-ParTUT', 'UD_Italian-PoSTWITA', 'UD_Italian-PUD',
+                                                                             'UD_Italian-Valico'])
+Fr_UD = CorpusLoader('French', tree_bank_path, mode='dev', corpus_filter=['UD_French-FQB', 'UD_French-ParisStories',
+                                                                          'UD_French-ParTUT', 'UD_French-PUD', 'UD_French-Rhapsodie',
+                                                                          ])
 En_have = extract_sentence_have(En_UD, ['have', 'had', 'has'],
                                 language='English',
                                 possessor='nsubj', possessee='obj')
