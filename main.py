7975ac5ae17c0c917e9cc0d85cd4c4eaf3c8bae6
Yuhang Zhu <969135594@qq.com>
2024-03-25 03:25:02 +0100

    with file write-in

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from Corpus_builder.Extract_pipeline import ExtractPipeline
 
 tree_bank_path = '/Users/yuhangzhu/Downloads/Universal Dependencies 2/ud-treebanks-v2.13'
+store_path = './data'
 pipeline = ExtractPipeline(tree_bank_path)
-print(len(pipeline.extract('French', 'dev', 'cmp', is_pos=False)))
+En_fut_pos_train = pipeline.extract('English', 'train', 'future', is_pos=True)
+En_fut_neg_train = pipeline.extract('English', 'train', 'future', is_pos=False)
+En_fut_pos_dev = pipeline.extract('English', 'dev', 'future', is_pos=True)
+En_fut_neg_dev = pipeline.extract('English', 'dev', 'future', is_pos=False)
+pipeline.file_write_in(En_fut_pos_dev, f'{store_path}/en_dev_fut_pos.json', 'English', 'dev')
+pipeline.file_write_in(En_fut_neg_dev, f'{store_path}/en_dev_fut_neg.json', 'English', 'dev')
+pipeline.file_write_in(En_fut_pos_train, f'{store_path}/en_train_fut_pos.json', 'English', 'train')
+pipeline.file_write_in(En_fut_neg_train, f'{store_path}/en_train_fut_neg.json', 'English', 'train')
