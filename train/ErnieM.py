ff31fd3e869f601f14457e00497a88fd94650281
Yuhang Zhu <969135594@qq.com>
2024-04-10 01:31:53 +0200

    new version

--- train/ErnieM.py
+++ train/ErnieM.py
@@ -4,7 +4,7 @@ import torch
 class erniem_pooling_representation(classfier_module):
   def __init__(self,
                pool_method: str='mean') -> None:
-    bert = self.__bert = ErnieMModel.from_pretrained('susnato/ernie-m-large_pytorch')
+    bert = ErnieMModel.from_pretrained('susnato/ernie-m-large_pytorch')
     super(erniem_pooling_representation, self).__init__(bert, 1024, 1, pool_method=pool_method)
 
   def forward(self,
--- train/english_bert.py
+++ train/english_bert.py
@@ -4,5 +4,5 @@ from classifier_module import classfier_module
 class english_bert(classfier_module):
     def __init__(self,
                 pool_method: str = 'mean') -> None:
-        bert = self.__bert = BertModel.from_pretrained('bert-large-cased')
+        bert = BertModel.from_pretrained('bert-large-cased')
         super(english_bert, self).__init__(bert, 1024, 1, pool_method=pool_method)
