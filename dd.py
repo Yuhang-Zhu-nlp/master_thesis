11dc8e0f91d4a5de763b2445a52b42436029830e
Yuhang Zhu <969135594@qq.com>
2024-07-26 18:07:31 +0200

    add data

--- dd.py
+++ dd.py
@@ -2,12 +2,12 @@ import json
 import random
 random.seed(20)
 a,b=[], []
-with open('data/en_dev_have_pos.json', 'r', encoding='utf-8') as fp:
+with open('data/zh_dev_have_neg.json', 'r', encoding='utf-8') as fp:
     a=json.load(fp)
-with open('data/zh_dev_have_pos.json', 'r', encoding='utf-8') as fp:
+with open('data/fi_dev_have_neg.json', 'r', encoding='utf-8') as fp:
     b=json.load(fp)
-a=random.sample(a, 83)
-#a.extend(random.sample(b, 83))
-a.extend(b)
-with open('en_dev_have_pos1.json', 'w', encoding='utf-8') as fp:
+a=random.sample(a, 13)
+a.extend(random.sample(b, 13))
+#a.extend(b)
+with open('zh_dev_have_neg1.json', 'w', encoding='utf-8') as fp:
     json.dump(a, fp, ensure_ascii=False)
