d11b7193e1c04a24d79613df79e7e0963fa977a5
Yuhang Zhu <969135594@qq.com>
2024-07-26 03:05:10 +0200

    add dataset

--- dd.py
+++ dd.py
@@ -2,12 +2,12 @@ import json
 import random
 random.seed(20)
 a,b=[], []
-with open('data/en_train_cmp_neg.json', 'r', encoding='utf-8') as fp:
+with open('data/en_train_cmp_pos.json', 'r', encoding='utf-8') as fp:
     a=json.load(fp)
-with open('data/sw_train_cmp_neg.json', 'r', encoding='utf-8') as fp:
+with open('data/fr_train_cmp_pos.json', 'r', encoding='utf-8') as fp:
     b=json.load(fp)
-a=random.sample(a, 467)
-a.extend(random.sample(b, 385))
-#a.extend(b)
-with open('en_train_cmp_neg1.json', 'w', encoding='utf-8') as fp:
+#a=random.sample(a, 69)
+#a.extend(random.sample(b, 47))
+a.extend(b)
+with open('fr_train_cmp_pos1.json', 'w', encoding='utf-8') as fp:
     json.dump(a, fp, ensure_ascii=False)
