e22b48bd726b61ba90797cb6a8a4659837bc7562
Yuhang Zhu <969135594@qq.com>
2024-07-26 18:00:07 +0200

    add data

--- dd.py
+++ dd.py
@@ -2,12 +2,12 @@ import json
 import random
 random.seed(20)
 a,b=[], []
-with open('data/en_train_cmp_pos.json', 'r', encoding='utf-8') as fp:
+with open('data/en_dev_have_pos.json', 'r', encoding='utf-8') as fp:
     a=json.load(fp)
-with open('data/fr_train_cmp_pos.json', 'r', encoding='utf-8') as fp:
+with open('data/zh_dev_have_pos.json', 'r', encoding='utf-8') as fp:
     b=json.load(fp)
-#a=random.sample(a, 69)
-#a.extend(random.sample(b, 47))
+a=random.sample(a, 83)
+#a.extend(random.sample(b, 83))
 a.extend(b)
-with open('fr_train_cmp_pos1.json', 'w', encoding='utf-8') as fp:
+with open('en_dev_have_pos1.json', 'w', encoding='utf-8') as fp:
     json.dump(a, fp, ensure_ascii=False)
